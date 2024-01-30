b77a695884132a1fef1024ab72c9b90f0ca85914
Jackson15951 <bsjackson@mavs.coloradomesa.edu>
2024-01-30 16:08:24 -0700

    dg

---
+++ Notes/Stuff.py
@@ -0,0 +1,12 @@
+hi = "Hello World!"
+a = "1"
+for i in range(3):
+    print(hi)
+    
+print(type(a))
+
+for i in range(2*5+int(a)):
+    print(i*2)
+    if (i == 10) == True:
+        print("Umm\n",10+15)
+        
