4b051fe115f25b4c2c5ec1fd59cca1cb88423b29
Jackson15951 <bsjackson@mavs.coloradomesa.edu>
2024-02-08 17:20:09 -0700

    triangle

---
+++ assignments/triangle/triangle.py
@@ -0,0 +1,48 @@
+'''
+Write programmer information and briefly describe the program at the top as comments (5 points)
+Write algorithm steps as comments. (10 points)
+The program must prompt the user to enter three sides of a triangle. (15 points)
+Calculate area of the given triangle. (20 points)
+Hint: Use Heron’s formula to calculate area given three sides
+Calculate perimeter of the given triangle. (20 points)
+Print the calculated values with proper descriptions. (10 points)
+Run the program multiple times to test and verify that the program results are correct. (10 points)
+(Bonus 10 points) Test if 3 sides entered actually form a triangle.
+Update README.md file with the status of the project and self-grade. (10 points) E.g.
+      ## 2 - Triangle
+    program finds area and perimeter of a triangle given three sides
+    all the requirements are completed
+    program tested at least 49 times
+    grade: 110/100
+
+'''
+
+
+
+
+import math
+
+# Prompt the user to enter the three sides of the triangle
+a, b, c, = input('Please input the three sides of the triangle: ').split(' ')
+
+# Converts it to float
+a, b, c, = float(a),float(b),float(c)
+
+# Finds half the perimeter
+halfp = float((a+b+c)/2)
+
+# Finds the area
+area = math.sqrt(halfp*(halfp-a)*(halfp-b)*(halfp-c))
+
+# Prints the area of the triangle
+print('The area of the triangle is ',area)
+
+# Prints the perimeter of the triangle
+print('The perimeter of the triangle is ',halfp*2)
+
+# Test if 3 sides entered actually form a triangle.
+if (a+b>=c) and (a+c>=b) and (b+c>=a):
+    print('This is a triangle')
+else:
+    print('this is not a triangle')
+
